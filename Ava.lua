4ada1b147f79e72ec2886b53009dbb5157e4c50e
Enter1he <cheifnon0905@gmail.com>
2020-07-03 17:55:39 +0500

    Delete Ava.lua

--- Ava.lua
+++
@@ -1,8 +0,0 @@
-Ava = { --rgbr standard
-	r = 36; -- width and it's channel(aic)
-	g = 54; -- height aic
-	b = 7; -- horizontal offset aic
-	r1= 9; -- vertical offset aic
-	pix = "Resources/St_Ava/Avatar1.png"; -- content
-};
-return Ava
